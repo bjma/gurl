81425542366927d4909181896955bba3eae182d8
bjma <bjma@ucsc.edu>
2021-09-10 13:06:51 -0700

    Added custom errors library

---
+++ handler/error.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Same thing as `log.Fatalln`
+func HandleError(err error) {
+    fmt.Printf("gurl: %s\n", err.Error())
+    os.Exit(1)
+}
+
+func NewError(s string) error {
+    return errors.New(s)
+}
